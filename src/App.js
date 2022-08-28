95d9c4a9f2172f307d6a9a7b7534c17fa3e445fc
Thais Rosa <tsrosa18@gmail.com>
2022-08-28 03:26:27 -0300

    corrigindo erros lint

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Card from './components/Card';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <h1>Tryunfo</h1>
         <Form />
         <Card />
-      </div>
+      </>
     );
   }
 }
