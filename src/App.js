d1e8dc86394b27a0e1f6aa5ca76b4a39d8b2cb4c
Thais Rosa <tsrosa18@gmail.com>
2022-08-29 18:19:01 -0300

    req 5

--- src/App.js
+++ src/App.js
@@ -19,12 +19,39 @@ class App extends React.Component {
   onInputChange = (event) => {
     const { name, checked, value } = event.target;
     const type = (name === 'cardTrunfo') ? checked : value;
-    this.setState({ [name]: type });
+    this.setState({ [name]: type }, this.buttonEnabler);
+  }; // The second parameter to setState() is an optional callback function that will be executed once setState is completed and the component is re-rendered.
+
+  buttonEnabler = () => { // disabled (boolean response) ... checa varificacçoes
+    const min = 0;
+    const max = 90;
+    const sum = 210;
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare } = this.state;
+    if (cardName !== ''
+      && cardDescription !== ''
+      && cardImage !== ''
+      && cardRare !== ''
+      && Number(cardAttr1) >= min
+      && Number(cardAttr1) <= max
+      && Number(cardAttr2) >= min
+      && Number(cardAttr2) <= max
+      && Number(cardAttr3) >= min
+      && Number(cardAttr3) <= max
+      && (Number(cardAttr1)
+      + Number(cardAttr2) + Number(cardAttr3)) <= sum) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   };
 
-  // onSaveButtonClick = () => {
-
-  // };
+  // onSaveButtonClick = () => { // execute this when click
 
   render() {
     const {
