6c610d350d3e4f812d195aa55eb2ec216e11f313
Thais Rosa <tsrosa18@gmail.com>
2022-09-01 11:50:17 -0300

    req 8

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      savedCards,
     } = this.state;
 
     return (
@@ -135,6 +136,23 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+
+        {
+          savedCards.map((element) => (
+            <Card
+              key={ element.cardName }
+              cardName={ element.cardName }
+              cardDescription={ element.cardDescription }
+              cardAttr1={ element.cardAttr1 }
+              cardAttr2={ element.cardAttr2 }
+              cardAttr3={ element.cardAttr3 }
+              cardImage={ element.cardImage }
+              cardRare={ element.cardRare }
+              cardTrunfo={ element.cardTrunfo }
+            />))
+
+        }
+
       </div>
     );
   }
